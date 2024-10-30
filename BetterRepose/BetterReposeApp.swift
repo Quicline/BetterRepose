cf628cd9a8d0c502eb064617144d6b0833ecf163
Armando Francisco <armando17171771@gmail.com>
2024-10-29 21:22:33 -0400

    Initial Commit

---
+++ BetterRepose/BetterReposeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterReposeApp.swift
+//  BetterRepose
+//
+//  Created by Armando Francisco on 10/29/24.
+//
+
+import SwiftUI
+
+@main
+struct BetterReposeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BetterReposeTests/BetterReposeTests.swift
@@ -0,0 +1,17 @@
+//
+//  BetterReposeTests.swift
+//  BetterReposeTests
+//
+//  Created by Armando Francisco on 10/29/24.
+//
+
+import Testing
+@testable import BetterRepose
+
+struct BetterReposeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
