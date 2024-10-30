f95dae40f9b1a6a70525efcf55115b8a7967c2f5
Armando Francisco <armando17171771@gmail.com>
2024-10-30 12:16:30 -0400

    basic layout and create Ml

--- BetterRepose/ContentView.swift
+++ BetterRepose/ContentView.swift
@@ -8,14 +8,39 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var sleepAmount = 8.0
+    @State private var wakeUp = Date.now
+    @State private var coffeAmount = 1
+    
+    func calculateBedtime() {
+            
+    }
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            VStack {
+                Text("When do you want to wake up?")
+                    .font(.headline)
+                
+                DatePicker("Please enter a time", selection: $wakeUp, displayedComponents: .hourAndMinute)
+                    .datePickerStyle(.automatic)
+                    .labelsHidden()
+                    
+                
+                Text("Desired amount of sleep")
+                    .font(.headline)
+                
+                Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...12, step: 0.25)
+                
+                Text("Daily coffee intake")
+                    .font(.headline)
+                    
+                Stepper(coffeAmount == 1 ? "\(coffeAmount) cup" : "\(coffeAmount) cups(s)", value: $coffeAmount, in: 1...20)
+            }
+            .toolbar {
+                Button("Calculate bedtime", action: calculateBedtime)
+            }
+            .navigationBarTitle("Better Repose")
         }
-        .padding()
     }
 }
 
